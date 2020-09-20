4a102737384b45b94228781f3a1b2a4d8d269914
bagusbima <53173441+bagusbima@users.noreply.github.com>
2020-09-20 15:41:46 +0700

    Add files via upload

---
+++ Hello Kotlin.kts
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "Gusbim"
+    val hobi = "Gaming"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
